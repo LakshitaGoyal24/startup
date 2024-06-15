4f2016e16d7d6b2c2a0d876d07bdd9a1e4a3b536
lakshita <laksita2004@gmail.com>
2024-06-15 16:23:47 +0530

    Updated server.js with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 const exp=require('express')
 const app=exp()
 require('dotenv').config()
+const mongoClient=require('mongodb').MongoClient
 
+mongoClient.connect(process.env.DB_URL)
+.then(client=>{
+    //get db obj
+    const blogdb=client.db('startupdb')
+    //get collection obj
+    //const startupCollection=blogdb.collection('startupCollection')
+    //share colelction obj with express app
+    //app.set('startupCollection',startupCollection)
+    //confirm db connection status
+    console.log("DB connection success")
+})
+.catch(err=>console.log("Err in DB connection",err))
 const port=process.env.PORT || 5000
 
 app.listen(port,()=>{console.log(`Server running on port ${port}`);})
